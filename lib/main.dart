ea8b2ec9fde9758c54f52b5351af9dcedc50d7e7
Arjun Rajbansh <arjun.rajbansh@gmail.com>
2021-06-23 15:38:52 +0530

    Recog add (WIP)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:handwritten_number_recognizer/recognizer_screen.dart';
+import 'package:handwritten_number_recognizer/screens/addition.dart';
 import 'package:handwritten_number_recognizer/screens/login.dart';
 import 'package:handwritten_number_recognizer/screens/options.dart';
 
@@ -13,7 +14,7 @@ class handwrittenNumberRecognizerApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Login_page(), //RecognizerScreen(title: 'Number recognizer',),
+      home: Addition(), //RecognizerScreen(title: 'Number recognizer',),
     );
   }
 }
--- lib/screens/addition.dart
+++ lib/screens/addition.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:handwritten_number_recognizer/constants.dart';
+import 'package:handwritten_number_recognizer/drawing_painter.dart';
+import 'package:handwritten_number_recognizer/brain.dart';
+
+import '../recognizer_screen.dart';
 
 class Addition extends StatelessWidget {
   @override
@@ -9,31 +14,39 @@ class Addition extends StatelessWidget {
     int randomNumber2 = random.nextInt(9) + 1;
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.red,
         appBar: AppBar(
           title: Center(
             child: Text("Arithmetic Addition"),
           ),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Container(
-          child: Text(
-            randomNumber1.toString() +
-                " + " +
-                randomNumber2.toString() +
-                " = ?",
-            style: TextStyle(
-              fontSize: 50,
-              color: Colors.black,
+        body: ListView(
+          children: [
+            Container(
+              child: Text(
+                randomNumber1.toString() +
+                    " + " +
+                    randomNumber2.toString() +
+                    " = ?",
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.black,
+                ),
+              ),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+              ),
             ),
-          ),
-          padding: EdgeInsets.all(20.0),
-          margin: EdgeInsets.all(20.0),
-          alignment: Alignment.center,
-          height: 200,
-          decoration: BoxDecoration(
-            color: Colors.blueGrey,
-          ),
+            Container(
+                child: RecognizerScreen(
+              title: 'Number recognizer',
+            )),
+          ],
         ),
       ),
     );
